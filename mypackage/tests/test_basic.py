411858df91888bcca188dc78641f43256163cef9
Max Heikenfeld <maxheikenfeld@web.de>
2020-03-29 12:34:56 +0200

    fixes to test_basic.py

--- mypackage/tests/test_basic.py
+++ mypackage/tests/test_basic.py
@@ -2,8 +2,8 @@ import pytest
 import mypackage
 
 def test_A():
-    assert mypackage.do_A()=="A"
+    assert mypackage.subpackage_A.do_A()=="A"
 
 
 def test_C():
-    assert mypackage.do_C()=='c'
+    assert mypackage.submodule_C.do_C()=='c'
